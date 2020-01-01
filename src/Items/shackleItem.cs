5beabfb6ced2a6b85bb306db9a1c56c8abae6868
Novocain1 <22160223+Novocain1@users.noreply.github.com>
2020-01-01 15:44:29 -0500

    make shacklegear do the rotation things temporal gear does

--- src/Items/shackleItem.cs
+++ src/Items/shackleItem.cs
@@ -1,17 +1,17 @@
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 
-namespace VSModLauncher.Items {
+namespace VSModLauncher.Items
+{
     public class shackleItem : Item
     {
-
         private ICoreServerAPI sapi;
-        
+
         public override void OnLoaded(ICoreAPI api)
         {
             base.OnLoaded(api);
-            if (api.Side != EnumAppSide.Server) return;
-            
             sapi = api as ICoreServerAPI;
         }
 
@@ -25,11 +25,10 @@ namespace VSModLauncher.Items {
             }
         }
 
-        public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel,
-            bool firstEvent, ref EnumHandHandling handling)
+        public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
             base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
-            
+
             if (slot.Itemstack.GetName() == "Shackle-Gear")
             {
                 byEntity.World.Logger.Debug("[SHACKLE-GEAR] OBJECT IN HAND IS IDENTIFIED");
@@ -39,6 +38,34 @@ namespace VSModLauncher.Items {
                 byEntity.World.Logger.Debug("[SHACKLE-GEAR] OBJECT FAILED");
             }
         }
+
+        public override void InGuiIdle(IWorldAccessor world, ItemStack stack)
+        {
+            GuiTransform.Rotation.Y = GameMath.Mod(world.ElapsedMilliseconds / 50f, 360);
+        }
+
+        public override void OnHeldIdle(ItemSlot slot, EntityAgent byEntity)
+        {
+            if (byEntity.World is IClientWorldAccessor)
+            {
+
+                FpHandTransform.Rotation.Y = GameMath.Mod(byEntity.World.ElapsedMilliseconds / 50f, 360);
+                TpHandTransform.Rotation.Y = GameMath.Mod(byEntity.World.ElapsedMilliseconds / 50f, 360);
+            }
+
+        }
+
+        public override void OnGroundIdle(EntityItem entityItem)
+        {
+            GroundTransform.Rotation.Y = GameMath.Mod(entityItem.World.ElapsedMilliseconds / 50f, 360);
+
+            if (entityItem.World is IClientWorldAccessor)
+            {
+                float angle = (entityItem.World.ElapsedMilliseconds / 15f + entityItem.EntityId * 20) % 360;
+                float bobbing = entityItem.Collided ? GameMath.Sin(angle * GameMath.DEG2RAD) / 15 : 0;
+                Vec3d pos = entityItem.LocalPos.XYZ;
+                pos.Y += 0.15f + bobbing;
+            }
+        }
     }
-    
 }
